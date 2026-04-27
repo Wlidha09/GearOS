a567692ae81eca078c5b9a5f212d7fb20f492409
Wlidha <wlidha09@gmail.com>
2026-04-27 18:14:19 +0100

    Fix final des modèles et suppression des doublons
    Cible .NET 8.0 et ajoute la classe DeviceKey
    
    Mise à jour du projet pour cibler .NET 8.0 au lieu de .NET 10.0. Ajout de la classe DeviceKey avec ses propriétés dans DeviceInfo.cs. Modification de la déclaration de la liste Keys pour utiliser le nom complet du type. Nettoyage mineur des commentaires et espaces.
    
    Cible .NET 8.0 et ajoute la classe DeviceKey
    
    Mise à jour du projet pour cibler .NET 8.0 au lieu de .NET 10.0. Ajout de la classe DeviceKey dans DeviceInfo.cs pour gérer les propriétés des touches. Utilisation explicite de System.Collections.Generic.List pour la liste Keys et nettoyage de commentaires/types redondants.

--- Models/DeviceInfo.cs
+++ Models/DeviceInfo.cs
@@ -2,15 +2,25 @@
 
 namespace GearOS.Models
 {
+    public class DeviceKey
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public KeyMapping Mapping { get; set; } = new KeyMapping();
+    }
+
+
     public class DeviceInfo
     {
         public string Name { get; set; }
         public string ImagePath { get; set; }
         public string Vendor { get; set; }
         public ushort VendorId { get; set; }
-        public int ProductId { get; set; } // Doit être un int ou ushort pour HidSharp
+        public int ProductId { get; set; }
         public string Category { get; set; }
-        public List<DeviceKey> Keys { get; set; } = new List<DeviceKey>();
+        public System.Collections.Generic.List<DeviceKey> Keys { get; set; } = new System.Collections.Generic.List<DeviceKey>();
         public DeviceProfile ActiveProfile { get; set; }
     }
 }
